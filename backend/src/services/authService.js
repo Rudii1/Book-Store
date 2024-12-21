21e4b7b9a443a9e8d4fa7ea1f4bb0e369c91d572
Fekete Rudolf Dominik <feketerudolf956@gmail.com>
2024-12-21 19:01:12 +0100

    Auth service added

---
+++ backend/src/services/authService.js
@@ -0,0 +1,53 @@
+const bycrpt = require("bcryptjs");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const createUser = async (userData) => {
+  try {
+    const email = userData.email;
+    const password = userData.password;
+    const firstName = userData.firstName;
+    const lastName = userData.lastName;
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      throw new Error("User already exists");
+    }
+    const hashedPassword = await bycrpt.hash(password, 10);
+    const user = new User({
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
+      role: "CUSTOMER",
+      password: hashedPassword,
+    });
+    await user.save();
+    return user;
+  } catch (error) {
+    console.error(`Error creating user: ${error}`);
+  }
+};
+
+const loginUser = async (userData) => {
+  try {
+    const email = userData.email;
+    const password = userData.password;
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      throw new Error("User not exists");
+    }
+    const isPasswordValid = await bycrpt.hash(password, existingUser.password);
+    if (isPasswordValid) {
+      throw new Error("Invalid password");
+    }
+    const token = jwt.sign(
+      { id: existingUser._id, role: existingUser.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    return token;
+  } catch (error) {
+    console.error(`Error creating user: ${error}`);
+  }
+};
+
+module.exports = { createUser, loginUser };
--- frontend/src/pages/auth/components/signup/Signup.js
+++ frontend/src/pages/auth/components/signup/Signup.js
@@ -1,143 +1,159 @@
 import {
-    Avatar,
-    Box,
-    Button,
-    CircularProgress,
-    Container,
-    CssBaseline,
-    TextField,
-    Typography,
-    Link,
-    Grid
-  } from '@mui/material';
-  import { createTheme, ThemeProvider } from '@mui/material/styles';
-  import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-  import { useState } from 'react';
-  import { useNavigate } from 'react-router-dom';
-  
-  const defaultTheme = createTheme();
-  
-  export default function Signup() {
-    const navigate = useNavigate();
-    const [formData, setFormData] = useState({
-      email: '',
-      firstName: '',
-      lastName: '',
-      password: ''
-    });
-    const [loading, setLoading] = useState(false);
-  
-    const handleInputChange = (event) => {
-      const { name, value } = event.target;
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: value
-      }));
-    };
-  
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      setLoading(true);
-      console.log(formData);
-      setLoading(false);
-    };
-  
-    const handleSignInClick = () => {
-      navigate('/login');
-    };
-  
-    return (
-      <ThemeProvider theme={defaultTheme}>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
+  Avatar,
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  CssBaseline,
+  TextField,
+  Typography,
+  Link,
+  Grid,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const defaultTheme = createTheme();
+
+export default function Signup() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    console.log(formData);
+    setLoading(false);
+  };
+
+  const handleSignInClick = () => {
+    navigate("/login");
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 6,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
           <Box
-            sx={{
-              marginTop: 6,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center'
-            }}
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign up
-            </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    autoComplete="given-name"
-                    name="firstName"
-                    required
-                    fullWidth
-                    id="firstName"
-                    label="First Name"
-                    autoFocus
-                    value={formData.firstName}
-                    onChange={handleInputChange}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="Last Name"
-                    name="lastName"
-                    autoComplete="family-name"
-                    value={formData.lastName}
-                    onChange={handleInputChange}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    value={formData.email}
-                    onChange={handleInputChange}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                    value={formData.password}
-                    onChange={handleInputChange}
-                  />
-                </Grid>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                  value={formData.firstName}
+                  onChange={handleInputChange}
+                />
               </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                disabled={
-                  !formData.email || !formData.firstName || !formData.lastName || !formData.password
-                }
-              >
-                {loading ? <CircularProgress color="success" size={24} /> : 'Sign Up'}
-              </Button>
-              <Grid container justifyContent="flex-end">
-                <Grid item>
-                  <Link variant="body2" onClick={handleSignInClick} sx={{ cursor: 'pointer' }}>
-                    Already have an account? Sign in
-                  </Link>
-                </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                  value={formData.lastName}
+                  onChange={handleInputChange}
+                />
               </Grid>
-            </Box>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              disabled={
+                !formData.email ||
+                !formData.firstName ||
+                !formData.lastName ||
+                !formData.password
+              }
+            >
+              {loading ? (
+                <CircularProgress color="success" size={24} />
+              ) : (
+                "Sign Up"
+              )}
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link
+                  variant="body2"
+                  onClick={handleSignInClick}
+                  sx={{ cursor: "pointer" }}
+                >
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
           </Box>
-        </Container>
-      </ThemeProvider>
-    );
-  }
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
